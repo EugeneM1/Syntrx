b5c7c83ff64b503059536e9eb2a72c98fb9d52e1
Eugene Mokri <eugene.mokri@gmail.com>
2026-05-01 12:44:28 -0700

    Add Vercel Analytics

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,22 +1,31 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Analytics } from "@vercel/analytics/react";
 import App from "./App";
 import Home from "./pages/Home";
 import Report from "./pages/Report";
 import Interactions from "./pages/Interactions";
+import Login from "./pages/Login";
+import Signup from "./pages/Signup";
+import { AuthProvider } from "./lib/auth";
 import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <BrowserRouter>
-      <Routes>
-        <Route element={<App />}>
-          <Route path="/" element={<Home />} />
-          <Route path="/report/:id" element={<Report />} />
-          <Route path="/interactions/:id" element={<Interactions />} />
-        </Route>
-      </Routes>
-    </BrowserRouter>
+    <AuthProvider>
+      <BrowserRouter>
+        <Routes>
+          <Route element={<App />}>
+            <Route path="/" element={<Home />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/signup" element={<Signup />} />
+            <Route path="/report/:id" element={<Report />} />
+            <Route path="/interactions/:id" element={<Interactions />} />
+          </Route>
+        </Routes>
+      </BrowserRouter>
+      <Analytics />
+    </AuthProvider>
   </React.StrictMode>,
 );
